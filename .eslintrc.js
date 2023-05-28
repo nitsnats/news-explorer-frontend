f351996c16fb85677ea7e1f2e559da978f8c4931
Nitsa Saban <nitsnats@hotmail.com>
2023-05-28 22:14:03 +0300

    fixing layout between 1050 to 1280

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,12 +22,12 @@ module.exports = {
         "no-empty": [0, "allow-empty-functions", "allow-empty-catch"],
         "react/jsx-uses-react": "error",
         "react/jsx-uses-vars": "error",
-        "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "indent": [
-            "error",
-            2
-        ]
+        // "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+        // "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        // "indent": [
+        //     "error",
+        //     2
+        // ]
         
     },
 }
