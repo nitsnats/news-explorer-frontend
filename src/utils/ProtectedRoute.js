0a72796895f38d940b17c243fc92f415b6450a70
Nitsa Saban <nitsnats@hotmail.com>
2023-06-10 22:01:47 +0300

    update ProtectedRoute4

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,36 +1,16 @@
-// import React from 'react';
-// import { Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
-// function ProtectedRoute({ component: Component, ...props }) {
-//   return (
-//     <Routes>
-//     <Route>
-//       {() =>
-//         props.isLoggedIn ? <Component {...props} /> : <Navigate to='./' />
-//       }
-//     </Route>
-//     </Routes>
-//   );
-// }
-
-// export default ProtectedRoute;
-
-import { Navigate } from "react-router-dom"
-
-
- function ProtectedRoute({ isLoggedIn, component: Component, ...props }) {
-
-
-
+function ProtectedRoute({ component: Component, ...props }) {
   return (
-    <>
-
-      {
-
-        isLoggedIn ? <Component {...props} isLoggedIn={isLoggedIn} /> : <Navigate to='./' />
-
+    <Routes>
+    <Route>
+      {() =>
+        props.isLoggedIn ? <Component {...props} /> : <Navigate to='/' />
       }
-    </>
-  )
+    </Route>
+    </Routes>
+  );
 }
+
 export default ProtectedRoute;
