4b9a6aaf2e5e6396b05880e60b4a966ad025979e
Nitsa Saban <nitsnats@hotmail.com>
2023-06-10 21:41:59 +0300

    update ProtectedRoute

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
 
 function ProtectedRoute({ component: Component, ...props }) {
   return (
