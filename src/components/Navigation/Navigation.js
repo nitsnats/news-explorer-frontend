c9a15457e03ac137642fc299b6dcbe6d28461aa0
Nitsa Saban <nitsnats@hotmail.com>
2023-06-11 10:00:19 +0300

    update Navigation3

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,60 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-// const Navigation = ({ isLoggedIn, navigationLink, isNavOpen }) => {
-//   function savedArticlesLink() {
-//     if (isLoggedIn) {
-//       return (
-//         <NavLink
-//           to='/saved-news'
-//           // exact={true}
-//           exact="true"
-//           activeClassName='navigation__link_active_dark'
-//           className={`navigation__link ${navigationLink(
-//             'navigation__link_dark'
-//           )}`}
-//         >
-//           Saved articles
-//         </NavLink>
-//       );
-//     } else {
-//       return;
-//     }
-//   }
-
-//   return (
-//     <div className={`navigation ${isNavOpen ? 'navigation_active' : ''}`}>
-//       <NavLink
-//         to='/'
-//         // exact={true}
-//         exact="true"
-//         activeClassName='navigation__link_active'
-//         className={`navigation__link ${navigationLink(
-//           'navigation__link_dark'
-//         )}`}
-//       >
-//         Home
-//       </NavLink>
-//       {savedArticlesLink()}
-//     </div>
-//   );
-// };
-
 const Navigation = ({ isLoggedIn, navigationLink, isNavOpen }) => {
-  function SavedArticlesLink() {
+  function savedArticlesLink() {
     if (isLoggedIn) {
       return (
         <NavLink
           to='/saved-news'
+          // exact={true}
           exact
           activeClassName='navigation__link_active_dark'
-          className={`navigation__link ${navigationLink('navigation__link_dark')}`}
+          className={`navigation__link ${navigationLink(
+            'navigation__link_dark'
+          )}`}
         >
           Saved articles
         </NavLink>
       );
     } else {
-      return; // Return null instead of an empty statement
+      return;
     }
   }
 
@@ -62,15 +26,51 @@ const Navigation = ({ isLoggedIn, navigationLink, isNavOpen }) => {
     <div className={`navigation ${isNavOpen ? 'navigation_active' : ''}`}>
       <NavLink
         to='/'
+        // exact={true}
         exact
         activeClassName='navigation__link_active'
-        className={`navigation__link ${navigationLink('navigation__link_dark')}`}
+        className={`navigation__link ${navigationLink(
+          'navigation__link_dark'
+        )}`}
       >
         Home
       </NavLink>
-      {SavedArticlesLink()}
+      {savedArticlesLink()}
     </div>
   );
 };
+
+// const Navigation = ({ isLoggedIn, navigationLink, isNavOpen }) => {
+//   function SavedArticlesLink() {
+//     if (isLoggedIn) {
+//       return (
+//         <NavLink
+//           to='/saved-news'
+//           exact
+//           activeClassName='navigation__link_active_dark'
+//           className={`navigation__link ${navigationLink('navigation__link_dark')}`}
+//         >
+//           Saved articles
+//         </NavLink>
+//       );
+//     } else {
+//       return; // Return null instead of an empty statement
+//     }
+//   }
+
+//   return (
+//     <div className={`navigation ${isNavOpen ? 'navigation_active' : ''}`}>
+//       <NavLink
+//         to='/'
+//         exact
+//         activeClassName='navigation__link_active'
+//         className={`navigation__link ${navigationLink('navigation__link_dark')}`}
+//       >
+//         Home
+//       </NavLink>
+//       {SavedArticlesLink()}
+//     </div>
+//   );
+// };
 
 export default Navigation;
