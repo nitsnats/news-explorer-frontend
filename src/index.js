b60e46cf6a2c6be63b0c8176e52048cb7c3ee279
Nitsa Saban <nitsnats@hotmail.com>
2023-06-13 13:29:35 +0300

    time to clean the roo..code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
-// import App from './components/App/App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// );
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -33,24 +16,6 @@ ReactDOM.createRoot(
 );
 
 reportWebVitals();
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
-// import App from './components/App/App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
